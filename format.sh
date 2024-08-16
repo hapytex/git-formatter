911dfe079be1e484ac01fa0ee59e32149f820ea6
Willem Van Onsem <yourfriends@hapytex.eu>
2024-08-16 19:43:52 +0200

    test-formatter

--- format.sh
+++ format.sh
@@ -7,10 +7,20 @@ format () {
     git ls-files "$pat" | xargs -- "$@" --
   fi
 }
+format () {
+  opar="$1"
+  shift
+  pat="$1"
+  shift
+  for f in `git ls-files --error-unmatch "$pat" 2>/dev/null`; do
+    "$@" -- "$f" "$opar" "$f"
+  done
+}
+
 
 format '*.hs' ormolu
 format '*.py' python3 -m isort --
 format '*.py' python3 -m black -- -S -q
-format '*.sql' python3 -m sqlformat -reindent -s -keywords upper -identifiers lower
+formatp -O '*.sql' python3 -m sqlformat -reindent -s -keywords upper -identifiers lower
 format '*.yaml' yamlfmt -w
 format '*.yml' yamlfmt -w
